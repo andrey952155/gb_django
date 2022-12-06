36505cea615315dbb74f5a01af497d45dc82dad6
andre344 <952155@mail.ru>
2022-12-07 00:42:20 +0700

    add lesson 3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,16 +3,17 @@ from mainapp.apps import MainappConfig
 
 from mainapp import views
 
-name = MainappConfig.name
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path('fpage', views.fpage, name='mainapp'),
+    # path('fpage', views.fpage, name='mainapp'),
     # path('<str:word>', views.durl),
-    path('ind', views.IndexPage.as_view()),
-    path('', views.MainPageView.as_view()),
-    path('contacts.html', views.ContactsPageView.as_view()),
-    path('courses_list.html', views.CourseslistPageView.as_view()),
-    path('doc_site.html', views.DocSitePageView.as_view()),
-    path('login.html', views.LoginPageView.as_view()),
-    path('news.html', views.NewsPageView.as_view()),
+    # path('ind', views.IndexPage.as_view()),
+    path('', views.MainPageView.as_view(), name='main_page'),
+    path('contacts/', views.ContactsPageView.as_view(), name='contacts'),
+    path('courses_list/', views.CourseslistPageView.as_view(), name='courses'),
+    path('doc_site/', views.DocSitePageView.as_view(), name='doc_site'),
+    path('login/', views.LoginPageView.as_view(), name='login'),
+    path('news/', views.NewsPageView.as_view(), name='news'),
+    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
@@ -42,3 +43,33 @@ class LoginPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        # context["news_title"] = "Громкий новостной заголовок"
+        # context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context['datetime'] = datetime.now()
+        context['news'] = [
+            {'news_title': f'Громкий новостной заголовок номер {i}',
+             'news_preview': 'Предварительное описание, которое заинтересует каждого'}
+            for i in range(1, 6)]
+        context['page_range'] = range(1, 5)
+        return context
+
+class NewsPageDetailView(TemplateView):
+    template_name = "mainapp/news_detail.html"
+
+    def get_context_data(self, pk=None, **kwargs):
+        context = super().get_context_data(pk=pk, **kwargs)
+        # context["news_object"] = get_object_or_404(News, pk=pk)
+        context["news_object"] = {'title': 'Красноречивый заголовок карточки', 'body':'Увлекательное содержание'}
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
