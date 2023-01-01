eae35ea9811823465269b22bd156bc49e7e5fd3c
andre344 <gveesst@gmail.com>
2023-01-01 23:33:58 +0700

    Доделанные недоделки из прошлого дз

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,12 @@ SECRET_KEY = 'django-insecure-6c-u7^h8be_3c@)rbt3@)=y_+sd+!07^1owa9uv^*d@z6hmst$
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+if DEBUG:
+    INTERNAL_IPS = [
+        "192.168.1.4",
+        "127.0.0.1",
+    ]
 
 # Application definition
 
@@ -35,12 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # "markdownify.apps.MarkdownifyConfig",
+    "markdownify.apps.MarkdownifyConfig",
 
     'mainapp.apps.MainappConfig',
     'authapp.apps.AuthappConfig',
     "crispy_forms",
-
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -162,3 +168,33 @@ LOGGING = {
         "mainapp": {"level": "INFO", "handlers": ["file", "console"]},
     },
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
+# Read about sending email:
+# https://docs.djangoproject.com/en/3.2/topics/email/
+# Full list of email settings:
+# https://docs.djangoproject.com/en/3.2/ref/settings/#email
+# EMAIL_HOST = "localhost"
+# EMAIL_PORT = "25"
+# For debugging: python -m smtpd -n -c DebuggingServer localhost:25
+# EMAIL_HOST_USER = "django@geekshop.local"
+# EMAIL_HOST_PASSWORD = "geekshop"
+# EMAIL_USE_SSL = False
+# If server support TLS:
+# EMAIL_USE_TLS = True
+# Email as files for debug
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = "var/email-messages/"
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin, UserPassesTestMixin,
 )
+from django.core.cache import cache
 from django.http import JsonResponse, FileResponse
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
@@ -15,6 +16,11 @@ from django.views.generic import (
     TemplateView,
     UpdateView,
     )
+
+from django.contrib import messages
+from django.http.response import HttpResponseRedirect
+from django.utils.translation import gettext_lazy as _
+from mainapp import tasks as mainapp_tasks
 from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
 from django.conf import settings
@@ -81,6 +87,13 @@ class CoursesDetailView(TemplateView):
             if not mainapp_models.CourseFeedback.objects.filter(course=context["course_object"], user=self.request.user).count():
                 context["feedback_form"] = mainapp_forms.CourseFeedbackForm(course=context["course_object"], user=self.request.user)
         context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(course=context["course_object"]).order_by("-created", "-rating")[:5]
+
+        cached_feedback = cache.get(f"feedback_list_{pk}")
+        if not cached_feedback:
+            context["feedback_list"] = (mainapp_models.CourseFeedback.objects.filter(course=context["course_object"]).order_by("-created", "-rating")[:5].select_related())
+            cache.set(f"feedback_list_{pk}", context["feedback_list"], timeout=300) # 5 minutes
+        else:
+            context["feedback_list"] = cached_feedback
         return context
 
 
@@ -96,6 +109,24 @@ class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
 
 class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactsPageView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["form"] = mainapp_forms.MailFeedbackForm(user=self.request.user)
+        return context
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            cache_lock_flag = cache.get(f"mail_feedback_lock_{self.request.user.pk}")
+            if not cache_lock_flag:
+                cache.set(f"mail_feedback_lock_{self.request.user.pk}", "lock", timeout=15,)
+                messages.add_message(self.request, messages.INFO, _("Message sended"))
+                print(self.request.POST.get("message"))
+                mainapp_tasks.send_feedback_mail.delay({"user_id": self.request.POST.get("user_id"), "message": self.request.POST.get("message"), })
+            else:
+                messages.add_message(self.request, messages.WARNING, _("You can send only one message per 5 minutes"),)
+        return HttpResponseRedirect(reverse_lazy("mainapp:contacts"))
 
 
 class DocSitePageView(TemplateView):
