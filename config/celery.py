24d6c9ba4b34bac9a0fe679d2120df39e990673b
andre344 <gveesst@gmail.com>
2023-01-07 00:58:11 +0700

    celery.py

--- config/celery.py
+++ config/celery.py
@@ -2,9 +2,10 @@ import os
 from django.conf import settings
 from celery import Celery
 
+
 if settings.DEBUG:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 celery_app = Celery("braniac")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
 celery_app.autodiscover_tasks()
-celery_app.autodiscover_tasks()
+
